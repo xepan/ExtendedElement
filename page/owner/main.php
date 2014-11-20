d0716e9462df996c4c6dff7d93cbd3cb70710a2c
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 14:52:07 +0530

    Icon added

--- page/owner/main.php
+++ page/owner/main.php
@@ -2,7 +2,9 @@
 
 class page_ExtendedElement_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
-		$this->add('H3')->setHTML('Component Owner Main Page<small>No Options At back end :)</small>');
+
+		$this->h1->setHTML('<i class="glyphicon glyphicon-resize-full"></i> '.$this->component_name. '<small>Extended Elements</small>');
+		$this->add('H3')->setHTML('<small>No Options At back end :)</small>');
 	}
 
 
